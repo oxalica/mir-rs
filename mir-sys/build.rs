08623ab4bda475c2d060935521bdc082a81fba07
oxalica <oxalicc@pm.me>
2025-03-04 21:31:04 -0500

    Update to mir v1.0.0 and switch to bindgen

--- mir-sys/build.rs
+++ mir-sys/build.rs
@@ -1,17 +1,25 @@
 use std::env;
 
+// NB: Sync with git submodule.
+const GIT_COMMIT: &str = "v1.0.0";
+
 fn main() {
+    println!("cargo::rerun-if-changed=mir");
+
+    if !cfg!(feature = "bundled") {
+        todo!();
+    }
+
     let mut build = cc::Build::new();
 
     if env::var("DEBUG").unwrap() == "false" {
         build.define("NDEBUG", "1");
     }
 
-    if env::var("CARGO_FEATURE_PARALLEL_GEN").is_ok() {
-        build.define("MIR_PARALLEL_GEN", "1");
-    }
-
+    // See: <https://github.com/vnmakarov/mir/blob/v1.0.0/GNUmakefile#L61>
     build
+        .define("GITCOMMIT", GIT_COMMIT)
+        .pic(true)
         .flag("-std=gnu11")
         .flag("-Wno-abi")
         .flag("-fsigned-char")
@@ -20,6 +28,45 @@ fn main() {
         .include("mir")
         .file("mir/mir.c")
         .file("mir/mir-gen.c")
-        .file("mir/c2mir/c2mir.c")
         .compile("mir");
+
+    println!("cargo::rustc-link-lib=m");
+    println!("cargo::rustc-link-lib=dl");
+
+    #[cfg(feature = "__internal_generate_sys")]
+    generate("src/bindings.rs");
+}
+
+#[cfg(feature = "__internal_generate_sys")]
+fn generate(out_path: &str) {
+    const HEADER_CONTENT: &str = r#"
+#include "mir/mir.h"
+"#;
+
+    bindgen::Builder::default()
+        .header_contents("wrapper.h", HEADER_CONTENT)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // Hide private funcs and types: <https://github.com/vnmakarov/mir/blob/v1.0.0/mir.h#L637>
+        .allowlist_var("MIR_.*")
+        .allowlist_type("MIR_.*")
+        .blocklist_type("MIR_code_reloc|MIR_code_reloc_t")
+        .allowlist_function("MIR_.*|_MIR_init|_MIR_get_api_version")
+        // Do not expand libc `FILE`.
+        .opaque_type("FILE")
+        .blocklist_type("FILE")
+        .raw_line("use libc::FILE;")
+        // No va_list support.
+        .blocklist_function("MIR_interp_arr_varg")
+        .blocklist_type(".*va_list.*")
+        // Fix f128 (long double) FFI-safety.
+        .raw_line("#[derive(Debug, Clone, Copy)] #[repr(C, align(16))] pub struct c_longdouble([u64; 2]); use c_longdouble as u128;")
+        // Manual tweaks.
+        .blocklist_type("MIR_imm_t|MIR_mem_t|MIR_op_t.*|MIR_insn|MIR_val_t")
+        .raw_line("use super::{MIR_op_t, MIR_insn, MIR_val_t};")
+        .prepend_enum_name(false)
+        .layout_tests(false)
+        .generate()
+        .expect("failed to bindgen")
+        .write_to_file(out_path)
+        .expect("failed to write bindgen output");
 }
